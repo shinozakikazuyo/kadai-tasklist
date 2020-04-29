7af003c9d6bc8c7d356abc1cb6d769a9bcfc498e
k-shi <kzysh05@gmail.com>
2020-04-29 09:08:50 +0000

    post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,22 +7,20 @@ class TasksController < ApplicationController
   #一覧を表示
   def index
     @tasks = current_user.tasks.order(id: :desc)
-	end
-	
-	#詳細ページ
-	def show
-	  @task = Task.find(params[:id])
-	end
-	
-	#新規作成ページ
-	def new
+  end
+  
+  #詳細ページ
+  def show
+  end
+  
+  #新規作成ページ
+  def new
     @task = Task.new
-	end
-	
-	#作成
-	def create
-    
-    @task = current_user.tasks.build(task_params)
+  end
+  
+  #作成
+  def create
+  @task = current_user.tasks.build(task_params)
     if @task.save
       flash[:success] = '正常に登録されました'
       redirect_to root_url
@@ -31,20 +29,15 @@ class TasksController < ApplicationController
       flash.now[:danger] = '登録できませんでした'
       #render 'tasks/index'
       render :new
-      
     end
-    
-	end
-	
-	#編集
-	def edit
-    @task = Task.find(params[:id])
-	end
-	
-	#更新
-	def update
-    @task = Task.find(params[:id])
-    
+  end
+  
+  #編集
+  def edit
+  end
+  
+  #更新
+  def update
     if @task.update(task_params)
       flash[:success] = "正常に更新されました"
       redirect_to @task
@@ -52,30 +45,28 @@ class TasksController < ApplicationController
       flash.now[:danger] = '更新できませんでした'
       render :edit
     end
-	end
-	
-	#削除
-	def destroy
-    @task = Task.find(params[:id])
+  end
+  
+  #削除
+  def destroy
     @task.destroy
     flash[:success] = "削除されました"
     redirect_to tasks_url
-	end
-	
-	
-	
-	private
-	
-	# Strong Parameter
-	def task_params
-		params.require(:task).permit(:content, :status)
-	end
-	
-	def check_user
-	  @task = current_user.tasks.find_by(id: params[:id])
-	  unless @task
-	    redirect_to root_url
-	  end
-	end
+  end
+  
+  
+  private
+  
+  # Strong Parameter
+  def task_params
+  	params.require(:task).permit(:content, :status)
+  end
+  
+  def check_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end
 	
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,14 +2,6 @@ class UsersController < ApplicationController
   
   #ログインしているかを確認する
   before_action :require_user_logged_in, only: [:index, :show]
-  
-  def index
-    @users = User.order(id: :desc)
-  end
-
-  def show
-    @user = User.find(params[:id])
-  end
 
   def new
     @user = User.new
