44ac6cee34b0654cdd0d5a2ffd3a3ffb1467d89f
k-shi <kzysh05@gmail.com>
2020-04-30 06:50:13 +0000

    扶養部分削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,4 @@
 class UsersController < ApplicationController
-  
-  #ログインしているかを確認する
-  before_action :require_user_logged_in, only: [:index, :show]
 
   def new
     @user = User.new
@@ -27,11 +24,6 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  end
-  
-  #新規会員情報を保持する
-  def new_login
-    User.find(session[:user_id])
   end
   
 end
